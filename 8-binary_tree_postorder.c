7bc473a6fa8f40579ae9c514c1a5a3b71c1c3d07
JorgeCal123/boom.git <git@github.com>
2022-03-23 19:20:44 -0700

    task 8 corr 2

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - function that goes through a binary tree using pre-order traversal
+ * binary_tree_postorder - function that goes through a binary
+ * tree using pre-order traversal
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node.
  */
@@ -13,4 +14,3 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
-
