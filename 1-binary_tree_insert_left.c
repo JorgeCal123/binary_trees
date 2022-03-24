c45cecb767f7766096eec0c67f803825ed037308
JorgeCal123/boom.git <git@github.com>
2022-03-23 19:35:28 -0700

    task 1 corr 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,26 +9,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
-	node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
-	if (!node || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	node->n = value;
-	if (parent->left == NULL)
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
 	{
-		node->parent = parent;
-		node->right = NULL;
-		parent->left = node;
+		new->left = parent->left;
+		parent->left->parent = new;
 	}
-	else
-	{
-		node->left = parent->left;
-		node->right = NULL;
-		node->parent = parent;
-		parent->left->parent = node;
-		parent->left = node;
-	}
-	return (node);
+	parent->left = new;
+
+	return (new);
 }
